6f9561d31c8be85d2430d3935a07cff8c75c5607
Rosalind H <rosalind.hung99@gmail.com>
2020-02-29 09:20:58 -0800

    shopping list add/check/remove functionality

---
+++ index.js
@@ -0,0 +1,34 @@
+function handleListActions(){
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        const newListItem = $('#shopping-list-entry').val();
+        if(newListItem === ''){
+            alert('Please enter an item to add!');
+            return;
+        }
+
+        $('#shopping-list-entry').val('');
+
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">${newListItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        event.preventDefault();
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event){
+        $(this).closest('li').remove();
+    });
+}
+
+$(handleListActions);
